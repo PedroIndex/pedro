700bf93f62db4261f888b1dced64500b7006c4bf
pedrok30k1 <130935765+PedroIndex@users.noreply.github.com>
2023-05-06 04:54:45 -0300

    oi

---
+++ Javascript/index.js
@@ -0,0 +1,10 @@
+function ip2() {
+    var tex1 =window.document.getElementById (`ip2`)
+    var tex2 =window.document.getElementById (`di2`)
+    var n1 = Number(tex1)
+    if(n1 > 60){
+       res.innerHTML = "olamudo"  
+    }
+    
+}
+ 
