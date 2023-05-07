6cba87e18e1b20c78c6709897e6b901d1b4b8b3d
pedrok30k1 <130935765+PedroIndex@users.noreply.github.com>
2023-05-07 12:51:26 -0300

    oi

---
+++ Javascript/index1/index1.js
@@ -0,0 +1,18 @@
+function calcular (){
+    var n1 = window.document.querySelector ('l11').valueAsNumber
+    var n2 = window.document.querySelector ('li2').valueAsNumber
+    var n3 = window.document.querySelector ('li3').valueAsNumber
+    if(com = / )  {
+
+    }
+    if(com = - ) {
+
+    }
+    if(com = *) {
+
+    }
+    if(com = +) {
+
+    }
+
+}
