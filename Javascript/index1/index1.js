1d988fb4461019e6e5538a5d176e575389aa8c94
pedrok30k1 <130935765+PedroIndex@users.noreply.github.com>
2023-05-21 23:54:55 -0300

    oi

--- Javascript/index1/index1.js
+++ Javascript/index1/index1.js
@@ -3,10 +3,10 @@ function calcular (){
     var n2 = window.document.querySelector ('li2').valueAsNumber
     var n3 = window.document.querySelector ('li3').valueAsNumber
     if(com = / )  {
-
+     var s n1 / n3
     }
     if(com = - ) {
-
+     var s n1
     }
     if(com = *) {
 
